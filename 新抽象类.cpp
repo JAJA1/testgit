2ebb34b1343c086545edd427135c31c04ed42146
xxm100200 <48643918+xxm100200@users.noreply.github.com>
2019-04-04 16:08:27 +0800

    徐熙铭

---
+++ 新抽象类.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+class qqq {
+	public:
+		qqq() = default;
+		qqq(const int a) :b(a) {}
+		virtual int p() const = 0;
+	private:
+		int b = 0;
+};
+class www:public qqq {
+public:
+	www() = default;
+	www(const int c) :d(c) {}
+	int p() const override;
+private:
+	int d = 0;
+};
+int main()
+{
+	www s;
+	return 0;
+}
