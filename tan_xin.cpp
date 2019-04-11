25cd0e90886380b8191decd1c4ad48ba07c13269
xxm100200 <48643918+xxm100200@users.noreply.github.com>
2019-04-11 18:42:53 +0800

    徐熙铭

---
+++ tan_xin.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#define N 5
+using namespace std;
+int mon[N] = { 1,2,5,10,20 };
+int chao[N] = { 4,4,2,2,1 };
+int shu(int m);
+int shu(int m)
+{
+	int r=0, i, h;
+	for (i = N - 1; i >= 0; i--) {
+		if (m / mon[i] < chao[i])
+			h = m / mon[i];
+		else
+			h = chao[i];
+		m -= h * mon[i];
+		r += h;
+	}
+	return r;
+}
+int main()
+{
+	int n, k;
+	cout << "需付钱数 ";
+	cin >> n;
+	k = shu(n);
+	cout <<"至少"<< k <<"张钞票"<< endl;
+}
